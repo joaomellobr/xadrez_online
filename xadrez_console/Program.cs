e41db537351056cb6237c4495192c321309ba63e
Joao Mello <joaomellobr@gmail.com>
2019-03-28 20:52:59 -0400

    Fazendo os primeiros testes de movimentacao

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -11,22 +11,23 @@ namespace xadrez_console
 
             try
             {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-               /* PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                Console.WriteLine(pos);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                Console.WriteLine(pos.toPosicao());*/
+                    partida.executaMovimento(origem, destino);
+                }
 
-                Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
-
-                Tela.imprimirTabuleiro(tab);
 
             }
             catch (TabuleiroException e)
--- xadrez_console/Tabuleiro/Peca.cs
+++ xadrez_console/Tabuleiro/Peca.cs
@@ -18,5 +18,9 @@ namespace tabuleiro
             this.tab = tab;
             this.qteMovimentos = 0;
         }
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
     }
 }
